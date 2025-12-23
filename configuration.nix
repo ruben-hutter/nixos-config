a26380a385b568e4b9eda534ca9d3d1a2d0b6b42
Ruben Hutter <ruben.hutter@rubenhutter.ch>
2025-12-23 23:26:04 +0100

    Add automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,15 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # === GARBAGE COLLECTION ===
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  nix.optimise.automatic = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data were taken. Don't change this!
   system.stateVersion = "25.11";
