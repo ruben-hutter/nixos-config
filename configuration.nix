67df08c9911da971f51896f171c45a20e5320bcb
Ruben Hutter <ruben.hutter@rubenhutter.ch>
2025-12-23 23:02:05 +0100

    Refactor minimal system config, user apps in home-manager

--- configuration.nix
+++ configuration.nix
@@ -1,38 +1,22 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
+  # === BOOT ===
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # === NETWORKING ===
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # === LOCALIZATION ===
   time.timeZone = "Europe/Zurich";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_GB.UTF-8";
     LC_IDENTIFICATION = "en_GB.UTF-8";
@@ -45,26 +29,22 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # === DESKTOP ENVIRONMENT ===
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
+  
+  # Keyboard
   services.xserver.xkb = {
     layout = "ch";
     variant = "fr";
   };
-
-  # Configure console keymap
   console.keyMap = "fr_CH";
 
-  # Enable CUPS to print documents.
+  # === HARDWARE ===
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  
+  # Sound
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -72,69 +52,26 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # === USER ===
   users.users.ruben = {
     isNormalUser = true;
     description = "ruben";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # === SYSTEM PACKAGES (minimal - only essential system tools) ===
   environment.systemPackages = with pkgs; [
-    vim
-    wget
-    neovim
-    git
+    git  # Needed for flakes
+    vim  # Minimal emergency editor
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
+  # === NIX SETTINGS ===
+  nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  # settings for stateful data were taken. Don't change this!
+  system.stateVersion = "25.11";
 }
--- home.nix
+++ home.nix
@@ -3,13 +3,69 @@
 {
   home.username = "ruben";
   home.homeDirectory = "/home/ruben";
-
   home.stateVersion = "25.11";
 
+  # === PACKAGES ===
   home.packages = with pkgs; [
     htop
+    btop
+    wget
+    curl
+    tree
+    ripgrep
+    fd
     firefox
+    vlc
+    libreoffice
   ];
+
+  # === PROGRAMS (with configuration) ===
+  
+  # Neovim
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+    
+    # You can add plugins later like:
+    # plugins = with pkgs.vimPlugins; [
+    #   vim-nix
+    #   nvim-treesitter
+    # ];
+  };
+
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "Ruben Hutter";
+    userEmail = "ruben.hutter@rubenhutter.ch";
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+    };
+  };
+
+  # Firefox (configured via home-manager)
+  programs.firefox = {
+    enable = true;
+    # You can add profiles and settings here:
+    # profiles.ruben = {
+    #   settings = {
+    #     "browser.startup.homepage" = "https://nixos.org";
+    #   };
+    # };
+  };
+
+  # Bash (or switch to zsh/fish later)
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -la";
+      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+      update = "cd ~/nixos-config && nix flake update && rebuild";
+    };
+  };
 
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
@@ -18,22 +74,7 @@
     # ".screenrc".source = dotfiles/screenrc;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ruben/etc/profile.d/hm-session-vars.sh
-  #
+  # === SESSION VARIABLES ===
   home.sessionVariables = {
     EDITOR = "nvim";
   };
