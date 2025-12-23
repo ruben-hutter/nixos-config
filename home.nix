60b7964bf8955d12bf127bf44b75cb75d8ef33d7
Ruben Hutter <ruben.hutter@rubenhutter.ch>
2025-12-23 22:37:35 +0100

    Initial flake setup with home-manager

---
+++ home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ruben";
+  home.homeDirectory = "/home/ruben";
+
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    htop
+    firefox
+  ];
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ruben/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+}
